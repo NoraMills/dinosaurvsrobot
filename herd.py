8f5bf3ea6c763cb2cba982032344fcf36669485a
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-07-15 10:56:27 -0500

    created herd class; added dinosaur names, damage, lists

---
+++ herd.py
@@ -0,0 +1,20 @@
+from dinosaur import Dinosaur
+
+
+class Herd:
+    def __init__(self):
+        self.dinosaur = []
+
+    def create_herd(self):
+        i = 0
+        dinosaurs = []
+        dinosaur_names = ["Rex Shortarm", "Michael Cera'Tops", "Longneck"]
+        damage_of_dinosaurs = [15, 25, 20]
+        while i < 3:
+            name = dinosaur_names[i]
+            dinosaur_damage = damage_of_dinosaurs[I]
+            dinosaur = Dinosaur(name, dinosaur_damage)
+            dinosaurs.append(dinosaur)
+            i += 1
+        self.dinosaurs = dinosaurs
+        pass
