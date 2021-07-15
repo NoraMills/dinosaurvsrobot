f106b6895e261a450e637f24c7428c7fa0404c64
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-07-15 11:06:27 -0500

    Created battlefield class; inserted method starting code

---
+++ battlefield.py
@@ -0,0 +1,32 @@
+from herd import Herd
+from fleet import Fleet
+
+
+class Battlefield:
+    def __init__(self):
+        self.Fleet = None
+        self.Herd = None
+
+    def run_game(self):
+        pass
+
+    def display_game(self):
+        pass
+
+    def battle(self):
+        pass
+
+    def dino_turn(self, dinosaur):
+        pass
+
+    def robo_turn(self, robot):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+
+    def show_robo_opponent_options(self):
+        pass
+
+    def display_winners(self):
+        pass
