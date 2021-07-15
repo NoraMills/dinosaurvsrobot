b80db998d5bb981cd52ecf2e88a38f916538bb62
clevernapkin <NORA.L.MILLS@GMAIL.COM>
2021-07-15 10:45:31 -0500

    dinosaur class created; dinorsaur attack against robot

---
+++ dinosaur.py
@@ -0,0 +1,11 @@
+class Dinosaur:
+
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.health = 100
+        self.attack_power = attack_power
+
+    def attack(self, robot):
+        robot.health = robot.health - self.attack_power
+        print(self.name + " has attacked. " + self.attack_power +
+              " damage was done to " + robot.name)
